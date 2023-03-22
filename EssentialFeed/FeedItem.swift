be80acf694f7ee900667751c9447381a664d1567
Leandro Fournier <leandro.fournier@gmail.com>
2023-03-21 22:09:37 -0300

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Leandro Fournier on 3/21/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
