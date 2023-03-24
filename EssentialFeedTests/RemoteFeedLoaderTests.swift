8281b1e088633741023a57ce16d14f5f29e59b9e
Leandro Fournier <leandro.fournier@gmail.com>
2023-03-24 15:34:38 -0300

    Fix deprecated Data initializer

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -69,7 +69,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
-        let invalidJSON = Data(bytes: "invalid json".utf8)
+        let invalidJSON = Data("invalid json".utf8)
         client.complete(withStatusCode: 200, data: invalidJSON)
         
         XCTAssertEqual(capturedErrors, [.invalidData])
