25732c1cee9ce60d96e961b3124e77af3575c1a2
Leandro Fournier <leandro.fournier@gmail.com>
2023-03-25 19:36:38 -0300

    Move HTTPClient to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Leandro Fournier on 3/25/23.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
