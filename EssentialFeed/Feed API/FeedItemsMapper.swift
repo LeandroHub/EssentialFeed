c7af96b3d9e1196d47df55312bedd9459bf57476
Leandro Fournier <leandro.fournier@gmail.com>
2023-03-25 21:02:23 -0300

    Swiftlint fix

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -11,7 +11,7 @@ internal final class FeedItemsMapper {
 
     private struct Root: Decodable {
         let items: [Item]
-        
+
         var feed: [FeedItem] {
             return items.map { $0.item }
         }
@@ -22,14 +22,14 @@ internal final class FeedItemsMapper {
         let description: String?
         let location: String?
         let image: URL
-        
+
         var item: FeedItem {
             return FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
 
     private static var OK_200: Int { return 200 }
-    
+
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(.invalidData)
