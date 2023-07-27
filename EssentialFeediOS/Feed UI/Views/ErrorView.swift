06a08e7370b8d998a43d522d58ed6f276e899f52
Leandro Fournier <leandro.fournier@gmail.com>
2023-07-27 14:52:49 -0300

    Move `ErrorView` to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Leandro Fournier on 7/27/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
