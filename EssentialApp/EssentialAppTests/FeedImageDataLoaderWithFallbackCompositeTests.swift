6ec01c22a8ceb6f9a5e2d34c4964739037d2c3c6
Leandro Fournier <leandro.fournier@gmail.com>
2023-08-04 19:22:55 -0300

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -9,13 +9,18 @@ import XCTest
 import EssentialFeed
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primaryLoader: FeedImageDataLoader
+
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primaryLoader = primary
+    }
+
     private class Task: FeedImageDataLoaderTask {
         func cancel() {}
     }
 
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {}
-
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeed.FeedImageDataLoaderTask {
+        _ = primaryLoader.loadImageData(from: url, completion: completion)
         return Task()
     }
 }
@@ -29,6 +34,24 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
 
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+
+    // MARK: - Helpers
+
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
     }
 
     private class LoaderSpy: FeedImageDataLoader {
