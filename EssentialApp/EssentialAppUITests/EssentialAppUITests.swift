12763b5cf61d769d51834709465a3561db7ccacc
Leandro Fournier <leandro.fournier@gmail.com>
2023-08-07 17:46:23 -0300

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialAppAcceptanceUITests.swift
+//  EssentialAppAcceptanceUITests
+//
+//  Created by Leandro Fournier on 8/7/23.
+//
+
+import XCTest
+
+final class EssentialAppAcceptanceUITests: XCTestCase { }
